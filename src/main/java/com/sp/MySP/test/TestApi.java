58dfb75e8568a6222fac1f4cc9cf3dbbe0d084e8
Santosh-Malkar-07 <santoshmalkar1001@gmail.com>
2026-03-23 20:39:53 +0530

    Added new spring boot project

---
+++ src/main/java/com/sp/MySP/test/TestApi.java
@@ -0,0 +1,14 @@
+package com.sp.MySP.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestApi {
+
+	@GetMapping("/welcome")
+	public String getData() {
+		
+		return "Welcome to SP Application dear user  Updated";
+	}
+}
---
+++ src/test/java/com/sp/MySP/MySpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.MySP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
